39153630cb15f3ac5ebb79906b0275715ff3355b
JakeTurnick <jaketurnick@gmail.com>
2023-02-10 12:24:42 -0500

    Notes updated to explin classes and Main file has some structure

---
+++ main.py
@@ -0,0 +1,19 @@
+# import rm1
+# import rm2
+# import rm3
+
+
+class Main_Game:
+    # starting game
+    def __init__(self):
+        pass
+
+    # while keys < 3:
+        # prompt choice
+
+    # if choice == "rm1"
+        # rm1() #imported
+    # elif choice == "rm2"
+        # rm2() #imported
+    # else
+        # rm3() #imported
