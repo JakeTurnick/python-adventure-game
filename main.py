52b3e77271248e56b6218ab36c286bc43cd49e87
Drew Coker <drewrosecoker@gmail.com>
2023-02-11 17:56:57 -0500

    update to intro story

--- main.py
+++ main.py
@@ -43,45 +43,60 @@ class Main_Game:
             ("You look around. \nYou're in your car but you've wrecked. \nAll you remember is you heard a 'pop' and you lost control. \nIt seems a tree limb pierced the windshield, cracking it... \n"), COLOR[0])
         time.sleep(3)
 
-    # starting choices:
-        choose_start = input(
-            "Would you like to inpsect yourself or the car first? (Type: MYSELF or CAR) \n")
-        if choose_start.upper() == "MYSELF":
-            print_slow(
-                ("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), COLOR[0])
-            time.sleep(3)
-    # name 'str' for other option (see 'choose_start_alt') w/ 'start_other'
-            start_other = "CAR"
-        elif choose_start.upper() == "CAR":
-            # left 'LOADING' to fill in later
-            print_slow(("You chose car...LOADING \n"), COLOR[0])
-            time.sleep(3)
-            start_other = "MYSELF"
-    # trying to figure out how we can loop this back to the start if player does not make a choice?
-        elif choose_start_alt != "MYSELF" or "CAR":
-            print_slow(
-                ("Please type MYSELF or CAR to continue... \n"), COLOR[0])
-            time.sleep(3)
-    # option to pick alternative option...
-        choose_start_alt = input(
-            f"Would I like to inpsect {start_other}? (Type: {start_other}) \n")
-        if choose_start_alt == "MYSELF":
-            print_slow(
-                ("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound\n"), COLOR[0])
-            time.sleep(3)
-        elif choose_start_alt == "CAR":
-            print_slow(("You chose car...LOADING \n"), COLOR[0])
-            time.sleep(3)
-        elif choose_start_alt != "MYSELF" or "CAR":
-            print_slow(("...LOADING \n"), COLOR[0])
-            time.sleep(3)
+        #list of starting choices:
+        car_or_myself = []
+        #run loop while len (length of list) < 2
+        while len(car_or_myself) < 2:
+            choose_start = input("Would you like to inspect yourself or the car? (Type: MYSELF or CAR) \n").upper()
+            if choose_start in ["MYSELF", "CAR"]:
+                if choose_start in car_or_myself:
+                    self.print_slow(("You have already selected " + choose_start + ".\n"), self.COLOR[0])
+                    time.sleep(3)
+                else:
+                    car_or_myself.append(choose_start)
+                    if choose_start == "MYSELF":
+                        self.print_slow(("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), self.COLOR[0])
+                        time.sleep(3)
+                    elif choose_start == "CAR":
+                        self.print_slow(("You chose car...LOADING \n"), self.COLOR[0])
+                        time.sleep(3)
+            else:
+                self.print_slow(("Please type 'MYSELF' or 'CAR' to continue... \n"), self.COLOR[0])
+                time.sleep(3)
 
-        choose_call_for_help = input(
-            f"You find your phone in the car. \nWho do you call first? (Options: 911 or LINDA, your wife...\n")
-        if choose_call_for_help == "911":
-            print("LOADING... \n")
-        elif choose_call_for_help == "LINDA":
-            print("LOADING... \n")
+        #call for help:
+        call_for_help = []
+        while len(call_for_help) < 2:
+            choose_call_for_help = input(f"You find your phone in the car. \nWho do you call? (Options: 911 or LINDA, your wife...\n").upper()
+            if choose_call_for_help in ["LINDA", "911"]:   
+                if choose_call_for_help in call_for_help:
+                    self.print_slow(("You have already selected " + choose_call_for_help + ".\n"), self.COLOR[0])
+                    time.sleep(3)
+                else:
+                    call_for_help.append(choose_call_for_help)
+                    if choose_call_for_help == "LINDA":
+                        print("LOADING...Linda...no response \n")
+                    elif choose_call_for_help == "911":
+                        print("LOADING...911...no response \n")
+            else:
+                self.print_slow(("Please type '911' or 'Linda' to continue... \n"), self.COLOR[0])
+                time.sleep(3)
+
+        self.print_slow(("No one answers the phone... \nYou notice the sun is setting, it's starting to get foggy... \n You also notice a small town in the distance... \n "), self.COLOR[0])
+
+        choose_wait_or_walk = ["WALK", "WAIT"]
+        wait_or_walk = input(f"Do you want to WALK to town or WAIT for help?\n").upper()
+        if wait_or_walk in choose_wait_or_walk:
+            if wait_or_walk == "WALK":      
+                self.print_slow(("You decide to walk to town...\n"), self.COLOR[0])
+                time.sleep(3)
+            if wait_or_walk == "WAIT":
+#IMPORTANT !! ADD OPTION TO END GAME HERE AFTER COMPLETE        
+                self.print_slow(("You decide to wait for help\nwaiting...\nwaiting...\nwaiting...\n...oh no...\na grizzly bear finds you and eats you! \nGAME OVER\n"), self.COLOR[0])
+                return
+        else:
+            self.print_slow(("Please choose 'WALK' or 'WAIT' to continue... \n"), self.COLOR[0])
+            time.sleep(3)
 
     def start_room(self):
         room_choice = input(
