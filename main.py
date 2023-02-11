ef6f7867b693e2dfd5f0430cf4063b597d80d8aa
JakeTurnick <jaketurnick@gmail.com>
2023-02-11 17:35:24 -0500

    added skip for testing at start of __init__, add your own rooms to test

--- main.py
+++ main.py
@@ -1,76 +1,87 @@
+import rm3
+import rm2
+from rm1 import rm1_class
 import time
 from colorama import init, Fore, Style
 init(autoreset=True)
-from rm1 import rm1_class
-import rm2
-import rm3
 
-#LOADING = no story here yet :(
+# LOADING = no story here yet :(
+
 
 class Main_Game:
     # starting game
 
-
     def __init__(self, key=0):
         self.key = key
-        
-    COLOR = [Fore.GREEN, Fore.RED, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
-        
-    #print_slow("message", COLOR['x']) slows the speed of text
-    #place index value from array above inside [] to choose color
-    def print_slow(self, message, COLOR):
-        for c in message:
-            print(COLOR + c, end='', flush=True)
-    #change speed of text below -> set to 0.1 for live review
-            time.sleep(0.01)
-    print(Style.RESET_ALL, end='', flush=True)
 
-    #intro text
-    def print_intro(self):
-        self.print_slow(("You slowly come too... \nyou've been unconsious for some time... \nHow long, you're not sure...  \n"), self.COLOR[0])
+        COLOR = [Fore.GREEN, Fore.RED, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+
+        skip = input("would you like to skip intro (for testing)? (y/n) : ")
+        if skip == "y":
+            room_sel = input("which room would you like to test (1, 2, 3) : ")
+            if room_sel == "3":
+                print("keys before rm3: ", self.key)
+                rm3.Garage.play_garage(self.key)  # imported
+                print("keys after rm3: ", self.key)
+
+    # print_slow("message", COLOR['x']) slows the speed of text
+    # place index value from array above inside [] to choose color
+
+        def print_slow(message, COLOR):
+            for c in message:
+                print(COLOR + c, end='')
+    # change speed of text below -> set to 0.1 for live review
+                time.sleep(0.01)
+        print(Style.RESET_ALL, end='')
+
+    # intro text
+        print_slow(
+            ("You slowly come too, you've been unconsious for some time. \nHow long, you're not sure...  \n"), COLOR[0])
         time.sleep(3)
 
-        self.print_slow(("You look around... \nYou're in your car but you've wrecked. \nAll you remember is you heard a 'pop' and you lost control... \nIt seems a tree limb pierced the windshield, cracking it... \n"), self.COLOR[0])
+        print_slow(
+            ("You look around. \nYou're in your car but you've wrecked. \nAll you remember is you heard a 'pop' and you lost control. \nIt seems a tree limb pierced the windshield, cracking it... \n"), COLOR[0])
         time.sleep(3)
 
-        #list of starting choices:
-        car_or_myself = []
-        #run loop while len (length of list) < 2
-        while len(car_or_myself) < 2:
-            choose_start = input("Would you like to inspect yourself or the car? (Type: MYSELF or CAR) \n").upper()
-            if choose_start in ["MYSELF", "CAR"]:
-                if choose_start in car_or_myself:
-                    self.print_slow(("You have already selected " + choose_start + ".\n"), self.COLOR[0])
-                    time.sleep(3)
-                else:
-                    car_or_myself.append(choose_start)
-                    if choose_start == "MYSELF":
-                        self.print_slow(("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), self.COLOR[0])
-                        time.sleep(3)
-                    elif choose_start == "CAR":
-                        self.print_slow(("You chose car...LOADING \n"), self.COLOR[0])
-                        time.sleep(3)
-            else:
-                self.print_slow(("Please type 'MYSELF' or 'CAR' to continue... \n"), self.COLOR[0])
-                time.sleep(3)
+    # starting choices:
+        choose_start = input(
+            "Would you like to inpsect yourself or the car first? (Type: MYSELF or CAR) \n")
+        if choose_start.upper() == "MYSELF":
+            print_slow(
+                ("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), COLOR[0])
+            time.sleep(3)
+    # name 'str' for other option (see 'choose_start_alt') w/ 'start_other'
+            start_other = "CAR"
+        elif choose_start.upper() == "CAR":
+            # left 'LOADING' to fill in later
+            print_slow(("You chose car...LOADING \n"), COLOR[0])
+            time.sleep(3)
+            start_other = "MYSELF"
+    # trying to figure out how we can loop this back to the start if player does not make a choice?
+        elif choose_start_alt != "MYSELF" or "CAR":
+            print_slow(
+                ("Please type MYSELF or CAR to continue... \n"), COLOR[0])
+            time.sleep(3)
+    # option to pick alternative option...
+        choose_start_alt = input(
+            f"Would I like to inpsect {start_other}? (Type: {start_other}) \n")
+        if choose_start_alt == "MYSELF":
+            print_slow(
+                ("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound\n"), COLOR[0])
+            time.sleep(3)
+        elif choose_start_alt == "CAR":
+            print_slow(("You chose car...LOADING \n"), COLOR[0])
+            time.sleep(3)
+        elif choose_start_alt != "MYSELF" or "CAR":
+            print_slow(("...LOADING \n"), COLOR[0])
+            time.sleep(3)
 
-        #call for help:
-        call_for_help = []
-        while len(call_for_help) < 2:
-            choose_call_for_help = input(f"You find your phone in the car. \nWho do you call? (Options: 911 or LINDA, your wife...\n").upper()
-            if choose_call_for_help in ["LINDA", "911"]:   
-                if choose_call_for_help in call_for_help:
-                    self.print_slow(("You have already selected " + choose_call_for_help + ".\n"), self.COLOR[0])
-                    time.sleep(3)
-                else:
-                    call_for_help.append(choose_call_for_help)
-                    if choose_call_for_help == "LINDA":
-                        print("LOADING...Linda... \n")
-                    elif choose_call_for_help == "911":
-                        print("LOADING...911... \n")
-            else:
-                self.print_slow(("Please type '911' or 'Linda' to continue... \n"), self.COLOR[0])
-                time.sleep(3)
+        choose_call_for_help = input(
+            f"You find your phone in the car. \nWho do you call first? (Options: 911 or LINDA, your wife...\n")
+        if choose_call_for_help == "911":
+            print("LOADING... \n")
+        elif choose_call_for_help == "LINDA":
+            print("LOADING... \n")
 
     def start_room(self):
         room_choice = input(
@@ -82,7 +93,9 @@ class Main_Game:
         elif room_choice == "rm2":
             rm2.rm2.play_room_two()  # imported
         else:
-            rm3.rm3.play_room_three()  # imported
+            print("keys before rm3: ", self.key)
+            rm3.Garage.play_garage(self.key)  # imported
+            print("keys after rm3: ", self.key)
 
     # while keys < 3:
         # prompt choice
