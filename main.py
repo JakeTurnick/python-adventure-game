e358a296a68d17d0656bb87fee786d24654c5783
Drew Coker <drewrosecoker@gmail.com>
2023-02-11 17:25:48 -0500

    refactored print_intro

--- main.py
+++ main.py
@@ -20,52 +20,57 @@ class Main_Game:
     #place index value from array above inside [] to choose color
     def print_slow(self, message, COLOR):
         for c in message:
-            print(COLOR + c, end='')
+            print(COLOR + c, end='', flush=True)
     #change speed of text below -> set to 0.1 for live review
             time.sleep(0.01)
-    print(Style.RESET_ALL, end='')
+    print(Style.RESET_ALL, end='', flush=True)
 
     #intro text
     def print_intro(self):
-        self.print_slow(("You slowly come too, you've been unconsious for some time. \nHow long, you're not sure...  \n"), self.COLOR[0])
+        self.print_slow(("You slowly come too... \nyou've been unconsious for some time... \nHow long, you're not sure...  \n"), self.COLOR[0])
         time.sleep(3)
 
-        self.print_slow(("You look around. \nYou're in your car but you've wrecked. \nAll you remember is you heard a 'pop' and you lost control. \nIt seems a tree limb pierced the windshield, cracking it... \n"), self.COLOR[0])
+        self.print_slow(("You look around... \nYou're in your car but you've wrecked. \nAll you remember is you heard a 'pop' and you lost control... \nIt seems a tree limb pierced the windshield, cracking it... \n"), self.COLOR[0])
         time.sleep(3)
 
-        #starting choices:
-        choose_start = input("Would you like to inpsect yourself or the car first? (Type: MYSELF or CAR) \n")
-        if choose_start.upper() == "MYSELF":
-            self.print_slow(("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), self.COLOR[0])
-            time.sleep(3)
-    #name 'str' for other option (see 'choose_start_alt') w/ 'start_other'
-            start_other = "CAR"
-        elif choose_start.upper() == "CAR":
-    #left 'LOADING' to fill in later 
-            self.print_slow(("You chose car...LOADING \n"), self.COLOR[0])
-            time.sleep(3)
-            start_other = "MYSELF"
-    #trying to figure out how we can loop this back to the start if player does not make a choice?
-        elif choose_start_alt != "MYSELF" or "CAR":
-            self.print_slow(("Please type MYSELF or CAR to continue... \n"), self.COLOR[0])
-            time.sleep(3)
-    #option to pick alternative option...
-        choose_start_alt = input(f"Would you like to inpsect {start_other}? (Type: {start_other}) \n")
-        if choose_start_alt == "MYSELF":
-            self.print_slow(("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound\n"), self.COLOR[0])
-            time.sleep(3)
-        elif choose_start_alt == "CAR":
-            self.print_slow(("You chose car...LOADING \n"), self.COLOR[0])
-            time.sleep(3)
-        elif choose_start_alt != "MYSELF" or "CAR":
-            self.print_slow(("...LOADING \n"), self.COLOR[0])
-            time.sleep(3)
+        #list of starting choices:
+        car_or_myself = []
+        #run loop while len (length of list) < 2
+        while len(car_or_myself) < 2:
+            choose_start = input("Would you like to inspect yourself or the car? (Type: MYSELF or CAR) \n").upper()
+            if choose_start in ["MYSELF", "CAR"]:
+                if choose_start in car_or_myself:
+                    self.print_slow(("You have already selected " + choose_start + ".\n"), self.COLOR[0])
+                    time.sleep(3)
+                else:
+                    car_or_myself.append(choose_start)
+                    if choose_start == "MYSELF":
+                        self.print_slow(("Your head hurts, you believe you have a concusion. \nYour thoughts are foggy and it's difficult to complete a coherent thought. \nYou have a small gash on your forehead. The bleeding has almost stopped. \nYou should find a gauze to wrap around the wound...\n"), self.COLOR[0])
+                        time.sleep(3)
+                    elif choose_start == "CAR":
+                        self.print_slow(("You chose car...LOADING \n"), self.COLOR[0])
+                        time.sleep(3)
+            else:
+                self.print_slow(("Please type 'MYSELF' or 'CAR' to continue... \n"), self.COLOR[0])
+                time.sleep(3)
 
-        choose_call_for_help = input(f"You find your phone in the car. \nWho do you call first? (Options: 911 or LINDA, your wife...\n")
-        if choose_call_for_help == "911":    
-            print("LOADING... \n")
-        elif choose_call_for_help == "LINDA":
-            print("LOADING... \n")
+        #call for help:
+        call_for_help = []
+        while len(call_for_help) < 2:
+            choose_call_for_help = input(f"You find your phone in the car. \nWho do you call? (Options: 911 or LINDA, your wife...\n").upper()
+            if choose_call_for_help in ["LINDA", "911"]:   
+                if choose_call_for_help in call_for_help:
+                    self.print_slow(("You have already selected " + choose_call_for_help + ".\n"), self.COLOR[0])
+                    time.sleep(3)
+                else:
+                    call_for_help.append(choose_call_for_help)
+                    if choose_call_for_help == "LINDA":
+                        print("LOADING...Linda... \n")
+                    elif choose_call_for_help == "911":
+                        print("LOADING...911... \n")
+            else:
+                self.print_slow(("Please type '911' or 'Linda' to continue... \n"), self.COLOR[0])
+                time.sleep(3)
 
     def start_room(self):
         room_choice = input(
